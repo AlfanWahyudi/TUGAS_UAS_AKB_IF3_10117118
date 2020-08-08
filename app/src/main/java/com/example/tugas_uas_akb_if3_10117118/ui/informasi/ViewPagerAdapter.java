8f86cb09365ccc1da9c5eb4aca49796663e4fbf3
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2020-08-08 10:18:20 +0700

    edit res

---
+++ app/src/main/java/com/example/tugas_uas_akb_if3_10117118/ui/informasi/ViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.tugas_uas_akb_if3_10117118.ui.informasi;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+/** NIM : 10117118
+ * Nama : Alfan Wahyudi
+ * Kelas : IF-3
+ * Tanggal : 12-05-2020**/
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private static int NUM_ITEMS = 3;
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return page1.newInstance(0);
+            case 1:
+                return page2.newInstance(1);
+            case 2:
+                return page3.newInstance(2);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+  /*  @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page " + position;
+    }*/
+
+
+
+}
---
+++ app/src/main/java/com/example/tugas_uas_akb_if3_10117118/ui/informasi/page2.java
@@ -0,0 +1,59 @@
+package com.example.tugas_uas_akb_if3_10117118.ui.informasi;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.tugas_uas_akb_if3_10117118.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link page2#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class page2 extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    // TODO: Rename and change types of parameters
+    private int page;
+
+    public page2() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     *
+     * @return A new instance of fragment page2.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static page2 newInstance(int page) {
+        page2 mPage2 = new page2();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        mPage2.setArguments(args);
+        return mPage2;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+        return inflater.inflate(R.layout.fragment_page2, container, false);
+    }
+}
